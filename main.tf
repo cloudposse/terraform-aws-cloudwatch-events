d235301372e178063ff8db03899020aef83507d2
Nuru <Nuru@users.noreply.github.com>
2020-12-17 15:46:36 -0800

    Better truncation for overly long rule names

--- main.tf
+++ main.tf
@@ -1,7 +1,16 @@
+module "rule_label" {
+  source  = "cloudposse/label/null"
+  version = "0.22.0" // requires Terraform >= 0.12.26
+
+  id_length_limit = 64
+
+  context = module.this.context
+}
+
 resource "aws_cloudwatch_event_rule" "this" {
-  name        = substr(module.this.id, 0, 63)
+  name        = module.rule_label.id
   is_enabled  = var.cloudwatch_event_rule_is_enabled
-  description = var.cloudwatch_event_rule_description != "" ? var.cloudwatch_event_rule_description : module.this.id
+  description = var.cloudwatch_event_rule_description != "" ? var.cloudwatch_event_rule_description : module.rule_label.id_full
 
   event_pattern = jsonencode(var.cloudwatch_event_rule_pattern)
 }
